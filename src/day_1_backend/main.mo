ce1ed9dd38739bdc6bec24f6b886467d45305811
Jose Antonio Lopez Zaccaro <jose@Gamer-Desktop.localdomain>
2023-05-09 11:15:06 -0300

    Ended day 1 project

--- src/day_1_backend/main.mo
+++ src/day_1_backend/main.mo
@@ -1,5 +1,80 @@
+import Float "mo:base/Float";
+// actor {
+// Ejercicios "Codign Challenges";
+//   var counter : Nat = 0;
+
+//   public query func greet(name : Text) : async Text {
+//     return "Hello, " # name # "!";
+//   };
+
+//   public query func multiply(n : Nat, m : Nat) : async Nat {
+//     return n * m;
+//   };
+
+//   public query func volume(n : Nat) : async Nat {
+//     return n * n * n;
+//   };
+
+//   public query func hours_to_minutes(n : Nat) : async Nat {
+//     return n * 60;
+//   };
+
+//   public func set_counter(n : Nat) : async () {
+//     counter := n;
+//   };
+
+//   public func get_counter() : async Nat {
+//     return counter;
+//   };
+
+//   public func test_divide(n : Nat, m : Nat) : async Bool {
+//     let r : Nat = n % m;
+//     let t : Bool = r == 0;
+//     return t;
+//   };
+
+//   public func is_even(n : Nat) : async Bool {
+//     let e : Bool = await test_divide(n, 2);
+//     return e;
+//   };
+// };
+
 actor {
-  public query func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+
+  var counter : Float = 0.0;
+
+  public shared func add(x : Float) : async Float {
+    counter += x;
+    return counter;
+  };
+  public shared func sub(x : Float) : async Float {
+    counter -= x;
+    return counter;
+  };
+  public shared func mul(x : Float) : async Float {
+    counter *= x;
+    return counter;
+  };
+  public shared func div(x : Float) : async Float {
+    counter /= x;
+    return counter;
+  };
+  public shared func reset() : async () {
+    counter := 0;
+  };
+  public shared query func see() : async Float {
+    return counter;
+  };
+  public shared func power(x : Float) : async Float {
+    counter := Float.pow(counter, x);
+    return counter;
+  };
+  public shared func sqrt() : async Float {
+    counter := Float.sqrt(counter);
+    return counter;
+  };
+  public shared func floor() : async Int {
+    counter := Float.floor(counter);
+    return Float.toInt(counter);
   };
 };
--- src/day_1_frontend/src/index.js
+++ src/day_1_frontend/src/index.js
@@ -1,19 +1,119 @@
 import { day_1_backend } from "../../declarations/day_1_backend";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
+// document.querySelector("form").addEventListener("submit", async (e) => {
+//   e.preventDefault();
+//   const button = e.target.querySelector("button");
 
-  const name = document.getElementById("name").value.toString();
+//   const name = document.getElementById("name").value.toString();
 
-  button.setAttribute("disabled", true);
+//   button.setAttribute("disabled", true);
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await day_1_backend.greet(name);
+//   // Interact with foo actor, calling the greet method
+//   const greeting = await day_1_backend.greet(name);
 
-  button.removeAttribute("disabled");
+//   button.removeAttribute("disabled");
 
-  document.getElementById("greeting").innerText = greeting;
+//   document.getElementById("greeting").innerText = greeting;
 
-  return false;
+//   return false;
+// });
+
+const buttonsContainer = document.getElementById('buttons-container');
+const operaciones = [{ 'add': day_1_backend.add }, { 'sub': day_1_backend.sub }, { 'mul': day_1_backend.mul }, { 'div': day_1_backend.div }, { '^': day_1_backend.power }, { '√': day_1_backend.sqrt }];
+const numeros = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0];
+
+const input = document.getElementById('pre-result');
+const resultado = document.getElementById('result');
+
+const botonesEspeciales = [
+  { etiqueta: '=', accion: calcularResultado },
+  { etiqueta: 'C', accion: borrarTodo }
+];
+
+window.addEventListener("DOMContentLoaded", async function () {
+  try {
+    resultado.value = await day_1_backend.see();
+  } catch (error) {
+    console.log(error);
+  }
+})
+
+
+
+function crearBoton(texto, onClick) {
+  const boton = document.createElement('button');
+  if (typeof texto == 'object') boton.textContent = Object.keys(texto)[0]
+  else boton.textContent = texto;
+  boton.addEventListener('click', onClick);
+  buttonsContainer.appendChild(boton);
+}
+
+async function agregarAlResultado(valor) {
+  if (input.value === '0') {
+    input.value = valor;
+  } else {
+    input.value += valor;
+  }
+  // console.log(input.value);
+
+}
+
+async function agregarOperador(operador) {
+
+  const operacion = operaciones.find(op => Object.keys(op)[0] == Object.keys(operador)[0])
+  console.log(operacion)
+  console.log(day_1_backend.sqrt)
+
+  if (operacion['√']) await operacion[Object.keys(operador)[0]]();
+  else await operacion[Object.keys(operador)[0]](parseFloat(input.value));
+
+  resultado.value = await day_1_backend.see()
+  input.value = 0
+}
+
+async function calcularResultado() {
+  const expresion = input.value;
+  let total;
+
+  try {
+
+    total = await day_1_backend.see()
+    console.log(total)
+    // eval(expresion);
+
+  } catch (error) {
+    total = 'Error';
+  }
+
+  resultado.value = total;
+  input.value = 0;
+}
+
+
+async function borrarTodo() {
+  try {
+    await day_1_backend.reset();
+    resultado.value = 0
+    input.value = 0
+    // console.log(input.value);
+
+    // resultado.value = '0';
+  } catch (error) {
+
+  }
+}
+
+// Generar botones para las operaciones
+operaciones.forEach(operacion => {
+  crearBoton(operacion, () => agregarOperador(operacion));
+});
+
+// Generar botones para los números
+numeros.forEach(numero => {
+  crearBoton(numero, () => agregarAlResultado(numero));
+});
+
+// Generar botones especiales
+botonesEspeciales.forEach(boton => {
+  crearBoton(boton.etiqueta, boton.accion);
 });
